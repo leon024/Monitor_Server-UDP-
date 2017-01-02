c92a516577260953d01aad6d477812dbc6dd1fab
leon024 <18900915666@163.com>
2017-01-02 16:13:59 +0800

    初始化

---
+++ UDP通信程序/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace UDP通信程序
+{
+    partial class UDPserverForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxSend = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxRe = new System.Windows.Forms.RichTextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.textBoxPortNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonUpdatePortNumber = new System.Windows.Forms.Button();
+            this.textBoxRemotePort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxHostIp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxRemoteIp = new System.Windows.Forms.TextBox();
+            this.checkBoxAppointIp = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBoxSend
+            // 
+            this.richTextBoxSend.Location = new System.Drawing.Point(12, 201);
+            this.richTextBoxSend.Name = "richTextBoxSend";
+            this.richTextBoxSend.Size = new System.Drawing.Size(327, 96);
+            this.richTextBoxSend.TabIndex = 0;
+            this.richTextBoxSend.Text = "";
+            this.richTextBoxSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBoxSend_KeyDown);
+            this.richTextBoxSend.KeyUp += new System.Windows.Forms.KeyEventHandler(this.richTextBoxSend_KeyDown);
+            // 
+            // richTextBoxRe
+            // 
+            this.richTextBoxRe.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBoxRe.Location = new System.Drawing.Point(13, 13);
+            this.richTextBoxRe.Name = "richTextBoxRe";
+            this.richTextBoxRe.ReadOnly = true;
+            this.richTextBoxRe.Size = new System.Drawing.Size(326, 169);
+            this.richTextBoxRe.TabIndex = 1;
+            this.richTextBoxRe.Text = "";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(264, 303);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "发送(回车)";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(171, 303);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.Text = "关闭";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // textBoxPortNumber
+            // 
+            this.textBoxPortNumber.Location = new System.Drawing.Point(528, 27);
+            this.textBoxPortNumber.Name = "textBoxPortNumber";
+            this.textBoxPortNumber.Size = new System.Drawing.Size(61, 21);
+            this.textBoxPortNumber.TabIndex = 4;
+            this.textBoxPortNumber.Text = "10101";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(526, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "本地端口：";
+            // 
+            // buttonUpdatePortNumber
+            // 
+            this.buttonUpdatePortNumber.Location = new System.Drawing.Point(528, 54);
+            this.buttonUpdatePortNumber.Name = "buttonUpdatePortNumber";
+            this.buttonUpdatePortNumber.Size = new System.Drawing.Size(61, 23);
+            this.buttonUpdatePortNumber.TabIndex = 6;
+            this.buttonUpdatePortNumber.Text = "修改";
+            this.buttonUpdatePortNumber.UseVisualStyleBackColor = true;
+            this.buttonUpdatePortNumber.Click += new System.EventHandler(this.buttonUpdatePortNumber_Click);
+            // 
+            // textBoxRemotePort
+            // 
+            this.textBoxRemotePort.Location = new System.Drawing.Point(528, 140);
+            this.textBoxRemotePort.Name = "textBoxRemotePort";
+            this.textBoxRemotePort.ReadOnly = true;
+            this.textBoxRemotePort.Size = new System.Drawing.Size(63, 21);
+            this.textBoxRemotePort.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(526, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "对方端口：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "本地IP：";
+            // 
+            // textBoxHostIp
+            // 
+            this.textBoxHostIp.Location = new System.Drawing.Point(360, 26);
+            this.textBoxHostIp.Name = "textBoxHostIp";
+            this.textBoxHostIp.ReadOnly = true;
+            this.textBoxHostIp.Size = new System.Drawing.Size(146, 21);
+            this.textBoxHostIp.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(360, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "对方IP：";
+            // 
+            // textBoxRemoteIp
+            // 
+            this.textBoxRemoteIp.Location = new System.Drawing.Point(360, 140);
+            this.textBoxRemoteIp.Name = "textBoxRemoteIp";
+            this.textBoxRemoteIp.ReadOnly = true;
+            this.textBoxRemoteIp.Size = new System.Drawing.Size(146, 21);
+            this.textBoxRemoteIp.TabIndex = 12;
+            // 
+            // checkBoxAppointIp
+            // 
+            this.checkBoxAppointIp.AutoSize = true;
+            this.checkBoxAppointIp.Location = new System.Drawing.Point(405, 123);
+            this.checkBoxAppointIp.Name = "checkBoxAppointIp";
+            this.checkBoxAppointIp.Size = new System.Drawing.Size(48, 16);
+            this.checkBoxAppointIp.TabIndex = 13;
+            this.checkBoxAppointIp.Text = "指定";
+            this.checkBoxAppointIp.UseVisualStyleBackColor = true;
+            this.checkBoxAppointIp.CheckedChanged += new System.EventHandler(this.checkBoxAppointIp_CheckedChanged);
+            // 
+            // UDPserverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 334);
+            this.Controls.Add(this.checkBoxAppointIp);
+            this.Controls.Add(this.textBoxRemoteIp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxHostIp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxRemotePort);
+            this.Controls.Add(this.buttonUpdatePortNumber);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPortNumber);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.richTextBoxRe);
+            this.Controls.Add(this.richTextBoxSend);
+            this.Name = "UDPserverForm";
+            this.Text = "UDP 服务端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxSend;
+        private System.Windows.Forms.RichTextBox richTextBoxRe;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.TextBox textBoxPortNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonUpdatePortNumber;
+        private System.Windows.Forms.TextBox textBoxRemotePort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxHostIp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxRemoteIp;
+        private System.Windows.Forms.CheckBox checkBoxAppointIp;
+    }
+}
+
---
+++ UDP通信程序/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace UDP通信程序
+{
+    public partial class UDPserverForm : Form
+    {
+        private UdpClient udpserver;//UDP服务器
+        private Thread udpListenThread;//UDP监听线程
+        private IPEndPoint remoteIpAndPort;//远程IP地址和端口
+        private delegate void displayMessageDelegate();//委托
+        public int kk = 0;
+        
+        public UDPserverForm()
+        {
+            InitializeComponent();            
+            udpListenThread = new Thread(new ThreadStart(udpListen));//创建监听线程
+            udpListenThread.IsBackground = true;//设为后台线程
+            udpListenThread.Start();//启动监听线程
+            //↓ 显示本机内网IPv4地址 win7系统
+            //this.textBoxHostIp.Text = Dns.GetHostEntry(Dns.GetHostName()).AddressList[3].ToString();
+
+            for(int i = 0; i < Dns.GetHostEntry(Dns.GetHostName()).AddressList.Length; i++)
+            {
+                //MessageBox.Show(Dns.GetHostEntry(Dns.GetHostName()).AddressList[i].ToString());
+                //↓ 获取本机的IPv4地址，不知道怎么获取，只找到了这个办法
+                if (Dns.GetHostEntry(Dns.GetHostName()).AddressList[i].AddressFamily.ToString().Equals("InterNetwork"))
+                {
+                    this.textBoxHostIp.Text = Dns.GetHostEntry(Dns.GetHostName()).AddressList[i].ToString();
+                    break;
+                }
+            }
+            textBoxHostIp.Text="121.42.185.160";
+        }
+
+        private void udpListen()//监听，线程的实际代码
+        {
+            int i = 1;
+            while (true)//循环，端口不可用自动加1
+            {
+                try
+                {
+                    udpserver = new UdpClient(int.Parse(this.textBoxPortNumber.Text));//创建UDP服务器，绑定要监听的端口
+                    break;                
+                }
+                catch
+                {
+                    //就是将预设的端口号加1
+                    this.textBoxPortNumber.Text = (int.Parse(this.textBoxPortNumber.Text)+i++).ToString();
+                }
+            }
+
+            remoteIpAndPort = new IPEndPoint(IPAddress.Any, 0);//定义IPENDPOINT，装载远程IP地址和端口                    
+            string receivedStr;//保存接收的数据字符的临时变量
+           
+ 
+            while (true)
+            {
+                try
+                {
+                    
+                    //将udpserver接受到指定的远程主机的数据包转换成字符串保存在临时变量
+                    byte[] by = udpserver.Receive(ref remoteIpAndPort);
+                    StringBuilder sb=new StringBuilder();
+                    for (int k = 0; k < by.Length; k++)
+                    {
+
+                        sb.Append(by[k].ToString("X2"));
+
+                    }                
+                    receivedStr =sb.ToString();
+                    string SID = receivedStr;
+                    SID = SID.Remove(2, 12);
+                    string str = receivedStr.Substring(6);
+                    string str1 = str.Remove(str.Length - 4, 4);
+
+                    double a = Convert.ToInt32(str1, 16);
+                    if (a > Convert.ToInt32("7FFF", 16))
+                        a = a - Convert.ToInt32("10000", 16);
+
+
+
+
+                    receivedStr = (a / 10).ToString();
+
+                    DateTime Daytemp = DateTime.Now;
+                    string year = Daytemp.Year.ToString();
+                    string month = null;
+                    string day = null;
+                    month = Daytemp.Month.ToString();
+                    day = Daytemp.Day.ToString();
+                    if (Daytemp.Month.ToString().Length == 1)
+                        month = "0" + Daytemp.Month.ToString();
+
+                    if (Daytemp.Day.ToString().Length == 1)
+                        day = "0" + Daytemp.Day.ToString();
+
+
+                    string filename= year + "_" + month + "_" + day+".txt";
+
+
+                    
+                     StreamWriter sw;
+                    if (Directory.Exists("D:/Monitor/"+SID+"/")== false)//如果不存在就创建file文件夹{
+                        Directory.CreateDirectory("D:/Monitor/" + SID + "/");
+
+
+                    if (File.Exists("D:/Monitor/" + SID + "/" + filename) == false)
+                    {
+                        File.Create("D:/Monitor/" + SID + "/" + filename).Close();//创建该文件
+                        sw = File.AppendText("D:/Monitor/" + SID + "/" + filename);
+
+                          sw.Write("{\"T\":[{\"time\":\"" + DateTime.Now.ToLongTimeString() + "\",\"temprature\":\"" + receivedStr + "\"}");
+                    sw.Close();
+                        
+                    }
+                    sw = File.AppendText("D:/Monitor/" + SID + "/" + filename);
+
+                    //sw.Write(Get_DateTime() + "<t>" + receivedStr + "</t>" + "\r\n");
+                        sw.Write(",{\"time\":\""+DateTime.Now.ToLongTimeString()+"\",\"temprature\":\""+receivedStr+"\"}");
+                    sw.Close();
+
+                    //定义委托
+                    displayMessageDelegate dis = delegate()
+                    {
+                        //string s = "\n[I received some data]";
+                        //byte[] b = System.Text.Encoding.UTF8.GetBytes(s);
+                        //this.udpserver.Send(b, b.Length, remoteIpAndPort);//接收到数据后返回 “[I received some data]”
+                        this.textBoxRemoteIp.Text = remoteIpAndPort.Address.ToString();//远程主机的IP显示到窗体
+                        this.textBoxRemotePort.Text = remoteIpAndPort.Port.ToString();//远程主机的端口号显示到窗体
+                        this.richTextBoxRe.AppendText(string.Format("\n{0}", remoteIpAndPort));//显示远程回话(当昵称用)
+                        this.richTextBoxRe.AppendText("\n"+receivedStr);//数据报显示到接收区域                        
+                        this.richTextBoxRe.ScrollToCaret();//滚动到最下面，显示最新消息
+                        //↓ 更新显示本地端口号(无实际意义)
+                        this.textBoxPortNumber.Text = ((IPEndPoint)(this.udpserver.Client.LocalEndPoint)).Port.ToString();
+                    };
+
+                    this.Invoke(dis);//执行委托
+                }
+                catch
+                {
+                    break;
+                }
+            }
+        }
+      
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            this.richTextBoxSend.Focus();
+            if (this.richTextBoxSend.Text.Length == 1)
+            {
+                this.richTextBoxSend.Clear();
+                return;
+            }
+            try
+            {
+                //将发送框中的文本转化成byte数组
+                //byte[] b = System.Text.Encoding.UTF8.GetBytes(this.richTextBoxSend.Text);
+                byte[] b = System.Text.Encoding.Default.GetBytes(this.richTextBoxSend.Text);
+                if (this.checkBoxAppointIp.Checked)
+                {
+                    remoteIpAndPort.Address = IPAddress.Parse(this.textBoxRemoteIp.Text);
+                    remoteIpAndPort.Port = int.Parse(this.textBoxRemotePort.Text);
+                }
+                this.udpserver.Send(b, b.Length, remoteIpAndPort);
+                this.richTextBoxRe.AppendText("\n" + this.textBoxHostIp.Text + ":" + this.textBoxPortNumber.Text);
+                this.richTextBoxRe.AppendText("\n"+this.richTextBoxSend.Text);
+                this.richTextBoxSend.Clear();
+                this.richTextBoxRe.ScrollToCaret();
+            }
+            catch
+            {
+                ;
+            }
+        }
+
+        //修改本地监听端口号则重启线程
+        private void buttonUpdatePortNumber_Click(object sender, EventArgs e)
+        {
+            udpserver.Close();//释放UDP连接
+            udpListenThread.Abort();//kill线程
+            //重建线程
+            udpListenThread = new Thread(new ThreadStart(udpListen));
+            udpListenThread.IsBackground = true;
+            udpListenThread.Start();
+        }
+
+        private void richTextBoxSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.buttonSend.PerformClick();
+            }
+        }
+
+        private void checkBoxAppointIp_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBoxAppointIp.Checked)
+            {
+                this.textBoxRemoteIp.ReadOnly = false;
+                this.textBoxRemotePort.ReadOnly = false;
+            }
+            else
+            {
+                this.textBoxRemoteIp.ReadOnly = true;
+                this.textBoxRemotePort.ReadOnly = true;
+            }
+        }
+    }
+}
